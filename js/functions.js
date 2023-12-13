a5b67289b915366e918050aae5205e1a8f6c5c3c
Daria Egarmina <d.egarmina@gmail.com>
2023-12-13 13:31:34 +0300

    Решает задачу с извлечением цифр

--- js/functions.js
+++ js/functions.js
@@ -21,3 +21,32 @@ function checkPalindrome () {
 }
 
 const result = checkPalindrome();
+
+
+// Возвращение цифр
+
+function getNumber (string) {
+
+  if (typeof string === 'number') {
+    return Math.abs(string.toString().replace(".", ""));
+  }
+
+  const formattedString = string.replaceAll(' ', '');
+
+  let resultString = '';
+  for (let i = 0; i <= formattedString.length - 1; i++) {
+    if (formattedString[i] >= 0 && formattedString[i] <= 9) {
+      resultString += formattedString[i];
+    }
+  }
+  return parseFloat(resultString);
+}
+
+console.log(getNumber('2023 год'));
+console.log(getNumber('ECMAScript 2022'));
+console.log(getNumber('1 кефир, 0.5 батона'));
+console.log(getNumber('агент 007'));
+console.log(getNumber('а я томат'));
+console.log(getNumber(2023));
+console.log(getNumber(-1));
+console.log(getNumber(1.5));
