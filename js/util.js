3d6607d37bf3e2290eb10f5137d3831be31175c3
Daria Egarmina <d.egarmina@gmail.com>
2024-01-12 13:29:27 +0300

    Создает структуру модулей

---
+++ js/util.js
@@ -0,0 +1,14 @@
+// Создаем случайное целое число из диапозона
+
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+// Получаем случайный элемент массива
+
+const getRandomArrayElement = (items) => items[getRandomInteger(0, items.length - 1)];
+
+export {getRandomInteger, getRandomArrayElement};
