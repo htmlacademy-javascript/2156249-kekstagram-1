6a6dbb0691e34412dfc03565cc823cd51e28bb48
Daria Egarmina <d.egarmina@gmail.com>
2024-02-19 15:54:22 +0300

    Вносит новые правки

--- js/picture.js
+++ js/picture.js
@@ -1,5 +1,3 @@
-import {createPhotos} from './data.js';
-
 const containerElement = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
@@ -9,7 +7,7 @@ const createPhotoElement = (photo) => {
   photoElement.querySelector('.picture__img').src = photo.url;
   photoElement.querySelector('.picture__img').alt = photo.description;
   photoElement.querySelector('.picture__likes').textContent = photo.likes;
-  photoElement.querySelector('.picture__comments').textContent = photo.comments.message;
+  photoElement.querySelector('.picture__comments').textContent = photo.comments.length;
 
   return photoElement;
 };
