745c9f1943792b5aefa9deb466c1bc8673f6c35b
Keks <keks@htmlacademy.ru>
2024-03-14 20:59:25 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,10 @@
+import {createPhotos} from './data.js';
+import {renderPhotos} from './render-photos.js';
+import { setGalleryListener } from './gallery.js';
+import './form.js';
+
+const userPhotos = createPhotos();
+
+renderPhotos(userPhotos);
+
+setGalleryListener(userPhotos);
