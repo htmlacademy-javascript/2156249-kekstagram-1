5009139506fc61c1265a506808b712a1b68d7ac8
Daria Egarmina <d.egarmina@gmail.com>
2024-04-03 12:45:53 +0300

    Делает 12 дз часть 2

--- js/util.js
+++ js/util.js
@@ -1,20 +1,5 @@
 const ALERT_SHOW_TIME = 5000;
 
-// Создаем случайное целое число из диапозона
-
-const getRandomInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-};
-
-// Получаем случайный элемент массива
-
-const getRandomArrayElement = (items) => items[getRandomInteger(0, items.length - 1)];
-
-// Определяем нажатие на клавишу Escape
-
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
 const showAlert = (message) => {
@@ -44,4 +29,4 @@ const debounce = (callback, timeoutDelay = 500) => {
   };
 };
 
-export {getRandomInteger, getRandomArrayElement, isEscapeKey, showAlert, debounce };
+export { isEscapeKey, showAlert, debounce };
